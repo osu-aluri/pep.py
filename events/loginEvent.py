388c1a98956196eff1a2e38e5e7f0a85a57911e2
KotypeyPyEdition <mr.unionchik@gmail.com>
2019-11-30 01:40:09 +0200

    fixes, again.......

--- events/loginEvent.py
+++ events/loginEvent.py
@@ -46,7 +46,7 @@ def handle(tornadoRequest):
 		splitData = loginData[2].split("|")
 		osuVersion = splitData[0]
 		timeOffset = int(splitData[1])
-		clientData = splitData[3].split(":")[:6]
+		clientData = splitData[3].split(":")[:5]
 
 		if len(clientData) < 4:
 			raise exceptions.forceUpdateException()
@@ -126,6 +126,7 @@ def handle(tornadoRequest):
 		
 
 		try:
+			minaseClient = splitData[6]
 			minaseClient = True
 		except Exception:
 			#log.logMessage(, discord="staff", of="info.txt", stdout=False)
